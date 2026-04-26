60f71a25c7d8d7fd9931aeea125e3df1d1a30446
Ronyfranzini84 <ro.franzini2@gmail.com>
2026-04-26 13:54:49 -0300

    Adicionando views dinamicas

--- filme/urls.py
+++ filme/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-from .views import homepage
+from .views import Homepage, Homefilmes
+
+app_name = 'filme'
 
 urlpatterns = [
-    path('', views.homepage, name='index'),
-    # suas outras rotas aqui...
+    path('', views.Homepage.as_view(), name='homepage'),
+    path('filmes/', views.Homefilmes.as_view(), name='homefilmes'),
+    path('filmes/<int:pk>/', views.DetalheFilme.as_view(), name='detalhes_filme'),
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Filme
+from django.views.generic import TemplateView, ListView, DetailView
 
+class Homepage(TemplateView):
+	template_name = "homepage.html"
 
-def homepage(request):
-	return render(request, "homepage.html")
+class Homefilmes(ListView):
+	model = Filme
+	template_name = "homefilmes.html"
+
+class DetalheFilme(DetailView):
+	model = Filme
+	template_name = "detalhesfilme.html"
