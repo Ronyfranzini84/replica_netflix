e047aea4d62387e5eadf931cb1debd96bddb500b
Ronyfranzini84 <ro.franzini2@gmail.com>
2026-05-01 22:05:30 -0300

    Ediatando a home filmes

--- filme/views.py
+++ filme/views.py
@@ -13,8 +13,14 @@ class DetalheFilme(DetailView):
 	model = Filme
 	template_name = "detalhesfilme.html"
 
+	def get(self, request, *args, **kwargs):
+		filme = self.get_object()
+		filme.visualizacoes += 1
+		filme.save()
+		return super().get(request, *args, **kwargs)
+
 	def get_context_data(self, **kwargs):
 		context = super(DetalheFilme, self).get_context_data(**kwargs)
-		filme_relacionados = Filme.objects.filter(categoria=self.get_object().categoria)
+		filme_relacionados = Filme.objects.filter(categoria=self.get_object().categoria) # type: ignore
 		context['filme_relacionados'] = filme_relacionados
 		return context
