6a4f50eb24692b5d19097527d9f576bf2e0b64e0
Ronyfranzini84 <ro.franzini2@gmail.com>
2026-05-03 14:16:01 -0300

    Criando Usuários personalizados

--- filme/models.py
+++ filme/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
@@ -31,5 +32,9 @@ class Episodio(models.Model):
 
     def __str__(self):
         return self.filme.titulo + " - " + self.titulo
+    
+
+class Usuario(AbstractUser):
+    filmes_vistos = models.ManyToManyField('Filme')
 
 
--- hashflix/settings.py
+++ hashflix/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'filme.Usuario'  # Especifica o modelo de usuário personalizado
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
